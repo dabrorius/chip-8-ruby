52b17f6a74db6158713168f008417950a265166e
Filip Defar <filip.defar@protonmail.com>
2025-02-22 11:19:09 +0100

    Add example ROM and try loading it

--- main.rb
+++ main.rb
@@ -1,1 +1,6 @@
-puts "Hello world!"
+rom_file = File.binread("./example_roms/1-chip8-logo.ch8")
+rom_hex_content = rom_file.unpack("H*").first
+
+puts "== ROM content =="
+puts rom_hex_content
+puts "== END =="

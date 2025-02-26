45fbca0756d785f803e0e31bcd3368eaacb70b16
Filip Defar <filip.defar@protonmail.com>
2025-02-26 06:46:46 +0100

    Load and attempt executing minimal sample rom

--- main.rb
+++ main.rb
@@ -1,6 +1,12 @@
+require "./executor"
+
 rom_file = File.binread("./example_roms/1-chip8-logo.ch8")
 rom_hex_content = rom_file.unpack("H*").first
 
 puts "== ROM content =="
 puts rom_hex_content
 puts "== END =="
+
+executor = Executor.new
+executor.load_program(rom_hex_content)
+executor.execute_program

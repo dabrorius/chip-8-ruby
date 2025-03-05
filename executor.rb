614caa5498c67d5d74ccc7c0d62a5a673b8be836
Filip Defar <filip.defar@protonmail.com>
2025-03-05 09:15:01 +0100

    Refactor commands

--- executor.rb
+++ executor.rb
@@ -54,27 +54,22 @@ class Executor
     command_hex_array = current_command.unpack("H*").first.chars.map { |digit| digit.to_i(16) }
 
     case command_hex_array
+    in [0, 0, 0xE, 0xE] # 00EE | RET | return from subroutine
+      execute_ret
     in [0, 0, 0xE, 0] # 00E0 | CLS | clears the display
       execute_cls
-      next_command
     in [1, n1, n2, n3] # 1NNN | JP | jump to location NNN
       execute_jp(n1 * 0x100 + n2 * 0x10 + n3)
     in [3, x, n1, n2] # 3XNN | SE | skip next command if register X is equal to NN
       execute_se(x, n1 * 0x10 + n2)
     in [6, x, n1, n2] # 6XNN | LD | loads register X with value NN
       execute_ld(x, n1 * 0x10 + n2)
-      next_command
     in [7, x, n1, n2] # 7XNN | ADD | adds value NN to register X
       execute_add(x, n1 * 0x10 + n2)
-      next_command
-    in [0, 0, 0xE, 0xE] # 00EE | RET | return from subroutine
-      execute_ret
     in [0xA, n1, n2, n3] # ANNN | ILD | loads I register with value NNN
       execute_ild(n1 * 0x100 + n2 * 0x10 + n3)
-      next_command
     in [0xD, x, y, n] # DXYN | DRW | draws sprite to display
       execute_drw(x, y, n)
-      next_command
     else
       fail "Reached unknown command #{command_hex_array.map { |n| n.to_s(16).upcase }.join }"
     end
@@ -84,40 +79,45 @@ class Executor
     first_part = @memory[@pc..@pc+1]
   end
 
-  def next_command
+  def next_command!
     @pc += COMMAND_SIZE
   end
 
-  def execute_se(position, value)
-    register_value = @registers.get(position)
-    next_command if register_value == value
-    next_command
+  # Commands implementation
+  def execute_ret
+    # For now set PC to nil to indicate end of program
+    @pc = nil
   end
 
-  def execute_ld(position, value)
-    @registers.set(position, value)
-  end
-
-  def execute_add(position, value)
-    current_value = @registers.get(position)
-    @registers.set(position, current_value + value)
+  def execute_cls
+    @display.clear
+    next_command!
   end
 
   def execute_jp(position)
     @pc = position
   end
 
-  def execute_ret
-    # For now set PC to nil to indicate end of program
-    @pc = nil
+  def execute_se(position, value)
+    register_value = @registers.get(position)
+    next_command! if register_value == value
+    next_command!
+  end
+
+  def execute_ld(position, value)
+    @registers.set(position, value)
+    next_command!
+  end
+
+  def execute_add(position, value)
+    current_value = @registers.get(position)
+    @registers.set(position, current_value + value)
+    next_command!
   end
 
   def execute_ild(value)
     @index_register = value
-  end
-
-  def execute_cls
-    @display.clear
+    next_command!
   end
 
   def execute_drw(register_x, register_y, n)
@@ -131,6 +131,7 @@ class Executor
         end
       end
     end
+    next_command!
   end
 
   def render

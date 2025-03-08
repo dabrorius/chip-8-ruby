eb0d52fa23bd72ef969e7d07be5b610002ed096d
Filip Defar <filip.defar@protonmail.com>
2025-03-08 18:55:55 +0100

    Extract other register manipulation commands

--- src/commands/register_manipulation.rb
+++ src/commands/register_manipulation.rb
@@ -4,12 +4,84 @@ require_relative "../const"
 #
 # @pc - program counter
 # @registers - Instance of Registers class
+# @vf_register
 #
 module Commands
   module RegisterManipulation
     def execute_add(position, value)
       current_value = @registers.get(position)
       @registers.set(position, current_value + value)
+      @pc += Const::COMMAND_SIZE
+    end
+
+    def execute_ld(position, value)
+      @registers.set(position, value)
+      @pc += Const::COMMAND_SIZE
+    end
+
+    def execute_ldr(register_x, register_y)
+      value_y = @registers.get(register_y)
+      @registers.set(register_x, value_y)
+      @pc += Const::COMMAND_SIZE
+    end
+
+    def execute_or(register_x, register_y)
+      value_x = @registers.get(register_x)
+      value_y = @registers.get(register_y)
+      @registers.set(register_x, value_x | value_y)
+      @pc += Const::COMMAND_SIZE
+    end
+
+    def execute_and(register_x, register_y)
+      value_x = @registers.get(register_x)
+      value_y = @registers.get(register_y)
+      @registers.set(register_x, value_x & value_y)
+      @pc += Const::COMMAND_SIZE
+    end
+
+    def execute_xor(register_x, register_y)
+      value_x = @registers.get(register_x)
+      value_y = @registers.get(register_y)
+      @registers.set(register_x, value_x ^ value_y)
+      @pc += Const::COMMAND_SIZE
+    end
+
+    def execute_radd(register_x, register_y)
+      value_x = @registers.get(register_x)
+      value_y = @registers.get(register_y)
+      raw_result = value_x + value_y
+      @vf_register = raw_result / 0x100
+      @registers.set(register_x, raw_result)
+      @pc += Const::COMMAND_SIZE
+    end
+
+    def execute_rsub(register_x, register_y)
+      value_x = @registers.get(register_x)
+      value_y = @registers.get(register_y)
+      @vf_register = value_x > value_y
+      @registers.set(register_x, value_x - value_y)
+      @pc += Const::COMMAND_SIZE
+    end
+
+    def execute_shr(register_x)
+      value_x = @registers.get(register_x)
+      @vf_register = value_x % 2
+      @registers.set(register_x, value_x / 2)
+      @pc += Const::COMMAND_SIZE
+    end
+
+    def execute_rsubn(register_x, register_y)
+      value_x = @registers.get(register_x)
+      value_y = @registers.get(register_y)
+      @vf_register = value_y > value_x
+      @registers.set(register_x, value_y - value_x)
+      @pc += Const::COMMAND_SIZE
+    end
+
+    def execute_shl(register_x)
+      value_x = @registers.get(register_x)
+      @vf_register = value_x * 2 > 0xFFFF
+      @registers.set(register_x, (value_x * 2) % 0xFFFF)
       @pc += Const::COMMAND_SIZE
     end
   end
